664a23a2c4cd9e6dca66aa74d89837dcdcbc0e32
eric.beland@nemoves.com <eric.beland@nemoves.com>
2011-06-20 09:36:07 -0400

    Cleanup

--- lib/scrapeybara/scraper.rb
+++
@@ -1,8 +0,0 @@
-class Scraper
-  
-  def initialize(opts = nil)
-    @outputter = opts[:outputter] || ObjectyHash.new # what to  
-    @result_outputter = opts[:result_outputter] || LogResultOutputter.new 
-  end   
-  
-end
