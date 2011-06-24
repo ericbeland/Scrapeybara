be3f6580fd9c70da5c9da8d0769d16d9f62c782b
eric.beland@nemoves.com <eric.beland@nemoves.com>
2011-06-24 15:59:15 -0400

    Bringing in capybara

--- lib/scrapeybara.rb
+++ lib/scrapeybara.rb
@@ -3,6 +3,7 @@ require_relative 'scrapeybara/scrape'
 require_relative 'scrapeybara/version'
 require_relative 'scrapeybara/params' 
 require_relative 'scrapeybara/step_results' 
+require_relative 'scrapeybara/capybara_extensions/capybara_extensions'
 
 require 'capybara'
 
@@ -10,13 +11,22 @@ require 'capybara'
 #require_relative 'scrapeybara/transaction_result'
 
 #require 'capybara'
-#require 'capybara/dsl'
+require 'capybara/dsl'
 
 # Capybara.run_server = false
 
 #Capybara.app_host = 'http://www.google.com'
 
 module Scrapeybara
+
+  include StepResults
+  include ErrorRecovery
+  include Scrape 
+  include Capybara
+  include Params
+  include CapybaraExtensions
+    
+  Capybara.default_driver = :selenium
   
   def log
     return @log if @log
@@ -27,10 +37,6 @@ module Scrapeybara
     @log = mylog
   end
   
-  include StepResults
-  include ErrorRecovery
-  include Scrape 
-  include Capybara
-  include Params
+
   
 end
