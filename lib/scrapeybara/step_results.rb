612cc0ad6f829fe4654674f214b578bc9931030e
eric.beland@nemoves.com <eric.beland@nemoves.com>
2011-06-29 11:30:52 -0400

    Fixed options passing to with_retry to pass in all options passed to the step

--- lib/scrapeybara/step_results.rb
+++ lib/scrapeybara/step_results.rb
@@ -1,10 +1,10 @@
 module StepResults
 
-  def step(step_name, options = {:retry => 0, })
+  def step(step_name, options = {})
     log.info("Starting #{step_name}") if log
     step_obj = Step.new(step_name) if defined?(Step)       
     duration_time = 0
-    results = with_retry(:retry => options[:retry]) do
+    results = with_retry(options) do
       start_time = Time.now  
       yield      
       duration_time = Time.now - start_time        
