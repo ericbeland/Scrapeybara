40b2009a12c21b0321917e1125b2f0525070016e
eric.beland@nemoves.com <eric.beland@nemoves.com>
2011-06-29 11:18:47 -0400

    fix wait to be retry_wait and set sleep to 0 by default

--- lib/scrapeybara/error_recovery.rb
+++ lib/scrapeybara/error_recovery.rb
@@ -1,7 +1,7 @@
 module ErrorRecovery  
     
   def with_retry(opts = {} , &block)
-    options = {:retry => 0, :wait => 10, :reset =>nil }.merge(opts)
+    options = {:retry => 0, :delay => nil, :retry_wait => 10, :reset =>nil }.merge(opts)
     results = []
     retries = 0
     until results.length > 0 and !(results.last.is_a?(Exception)) || (retries == options[:retry])
@@ -10,12 +10,14 @@ module ErrorRecovery
         results << yield        
       rescue Exception => e 
         results << e
-        unless retries < options[:retry]          
+        if retries < options[:retry]          
+          (options[:reset].call rescue nil) if options[:reset]      
+          sleep options[:retry_wait]
+        else          
           raise e
-        end        
+        end
+        sleep options[:delay] if options[:delay]
       end    
-      (options[:reset].call rescue nil) if options[:reset]      
-      sleep options[:wait]                      
     end
     results
   end      
